ceff49f921667fb8e654529fc2f5271b2d0897a8
zakariabizri <zakariabizri1@gmail.com>
2023-06-30 08:37:44 -0700

    Functions, nested loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,30 +2,31 @@
 
 /**
  * print_sign - prints the sign of a number
- * @n: the int to check
- * Return: 1 and prints + if n is greater than zero
- * 0 and prints 0 if n is zero
- * -1 and prints - if n is less than zero
+ * @n: the integer to check
+ *
+ * Return: 1 and prints '+' if n is greater than zero
+ *         0 and prints '0' if n is zero
+ *        -1 and prints '-' if n is less than zero
  */
 int print_sign(int n)
 {
-    if (n > 0)
-    {
-        _putchar('+');
-        return (1);
-    }
-    else if (n == 0)
-    {
-        _putchar(48);
-        return (0);
-    }
-    else if (n < 0)
-    {
-        _putchar('-');
-        return (-1);
-    }
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
 
-    /* Added return statement for the case when n is negative */
-    return (-1);
+	/* Added return statement for the case when n is negative */
+	return (-1);
 }
 
