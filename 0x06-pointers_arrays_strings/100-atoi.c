ba766b3acaa19918826ec63355d5c6baba1aea2e
zakariabizri <zakariabizri1@gmail.com>
2023-07-08 00:13:07 -0700

    Arguments in C

--- 0x06-pointers_arrays_strings/100-atoi.c
+++ 0x06-pointers_arrays_strings/100-atoi.c
@@ -3,10 +3,30 @@
 #include "100-atoi.h"
 
 /**
- * main - check the code
+ * _atoi - Convert a string to an integer
+ * @s: The string to convert
  *
- * Return: Always 0.
+ * Return: The converted integer
  */
+int _atoi(char *s)
+{
+	int sign = 1;
+	int result = 0;
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		else if (s[i] >= '0' && s[i] <= '9')
+			result = result * 10 + (s[i] - '0');
+		else if (result != 0)
+			break;
+		i++;
+	}
+
+	return (result * sign);
+}
 int main(void)
 {
     int nb;
